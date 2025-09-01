76938e2990128c949a6dbc217e1c94df0d9804ec
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2025-09-01 16:41:02 +0530

    k8-eksctl

--- workstation.sh
+++ workstation.sh
@@ -101,6 +101,9 @@ VALIDATE $? "Go to '/k8-eksctl' directory"
 eksctl create cluster --config-file=eks.yaml 
 VALIDATE $? "Installing ekscluster"
 
+aws eks update-kubeconfig --region us-east-1 --name roboshop
+
+
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver 
 helm upgrade --install aws-ebs-csi-driver \
     --namespace kube-system \
